33a5d9daa88697a81c023e78ddd02b8f10460c0c
stanstanstanlee <stanstanstanlee@gmail.com>
2025-12-04 15:39:26 +0900

    init

---
+++ src/test/java/com/example/instagram02/Instagram02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instagram02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Instagram02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
